36b058163a9d61748abd82b4d2bd6be472caeebb
ahaku <avznn@yandex.ru>
2022-04-26 12:33:51 +0300

    add assemblyscript loader

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { IExports } from "./types";
 import { canvas, context, runButton, stepButton } from "./ui";
+import loader from "@assemblyscript/loader";
 
 let isRunning = false;
 const RGB_ALIVE = 0xfd9925;
@@ -42,81 +43,76 @@ const importObject = {
     BGR_DEAD: rgb2bgr(RGB_DEAD) & ~1, // little endian, LSB must not be set
     BIT_ROT,
   },
-  Math,
+  // Math,
 };
+loader.instantiate(fetch("build/debug.wasm"), importObject).then((module) => {
+  const exports = module.exports as IExports;
+  let pressed = false;
 
-// @ts-ignore
-WebAssembly.instantiateStreaming(fetch("build/debug.wasm"), importObject).then(
-  (module) => {
-    const exports = module.instance.exports as IExports;
-    let pressed = false;
+  exports.init(width, height);
 
-    exports.init(width, height);
+  const memoryBuffer = new Uint32Array(memory.buffer);
 
-    const memoryBuffer = new Uint32Array(memory.buffer);
+  stepButton.onclick = () => {
+    memoryBuffer.copyWithin(0, size, size + size);
+    exports.step();
+  };
+  (function update() {
+    setTimeout(update, 1000 / 30);
+    if (isRunning) {
+      memoryBuffer.copyWithin(0, size, size + size); // copy output to input
+      exports.step(); // perform the next step
+    }
+  })();
 
-    stepButton.onclick = () => {
-      memoryBuffer.copyWithin(0, size, size + size);
-      exports.step();
-    };
+  // Keep rendering the output at [size, 2*size]
+  const imageData = context.createImageData(width, height);
+  const argb = new Uint32Array(imageData.data.buffer);
+  (function render() {
+    requestAnimationFrame(render);
+    argb.set(memoryBuffer.subarray(size, size + size)); // copy output to image buffer
+    context.putImageData(imageData, 0, 0); // apply image buffer
+  })();
 
-    (function update() {
-      setTimeout(update, 1000 / 30);
-      if (isRunning) {
-        memoryBuffer.copyWithin(0, size, size + size); // copy output to input
-        exports.step(); // perform the next step
+  // canvas.addEventListener("mousedown", ({ x, y }) => {
+  //   memoryBuffer.copyWithin(0, size, size + size);
+  //   exports.drawAtPos(x >>> SIZE_BIT_OFFSET, y >>> SIZE_BIT_OFFSET);
+  // });
+
+  [
+    [canvas, "mousedown"],
+    [canvas, "touchstart"],
+  ].forEach((eh: [HTMLCanvasElement, string]) =>
+    eh[0].addEventListener(eh[1], () => (pressed = true))
+  );
+  [
+    [document, "mouseup"],
+    [document, "touchend"],
+  ].forEach((eh: [Document, string]) =>
+    eh[0].addEventListener(eh[1], () => (pressed = false))
+  );
+  [
+    [canvas, "mousemove"],
+    [canvas, "touchmove"],
+    [canvas, "mousedown"],
+  ].forEach((eh: [HTMLCanvasElement, string]) =>
+    eh[0].addEventListener(eh[1], (e: Event | TouchEvent) => {
+      if (!pressed) return;
+      let event;
+      if ("touches" in e) {
+        if (e.touches.length > 1) return;
+        event = e.touches[0];
+      } else {
+        event = e;
       }
-    })();
-
-    // Keep rendering the output at [size, 2*size]
-    const imageData = context.createImageData(width, height);
-    const argb = new Uint32Array(imageData.data.buffer);
-    (function render() {
-      requestAnimationFrame(render);
-      argb.set(memoryBuffer.subarray(size, size + size)); // copy output to image buffer
-      context.putImageData(imageData, 0, 0); // apply image buffer
-    })();
-
-    // canvas.addEventListener("mousedown", ({ x, y }) => {
-    //   memoryBuffer.copyWithin(0, size, size + size);
-    //   exports.drawAtPos(x >>> SIZE_BIT_OFFSET, y >>> SIZE_BIT_OFFSET);
-    // });
-
-    [
-      [canvas, "mousedown"],
-      [canvas, "touchstart"],
-    ].forEach((eh: [HTMLCanvasElement, string]) =>
-      eh[0].addEventListener(eh[1], () => (pressed = true))
-    );
-    [
-      [document, "mouseup"],
-      [document, "touchend"],
-    ].forEach((eh: [Document, string]) =>
-      eh[0].addEventListener(eh[1], () => (pressed = false))
-    );
-    [
-      [canvas, "mousemove"],
-      [canvas, "touchmove"],
-      [canvas, "mousedown"],
-    ].forEach((eh: [HTMLCanvasElement, string]) =>
-      eh[0].addEventListener(eh[1], (e: Event | TouchEvent) => {
-        if (!pressed) return;
-        let event;
-        if ("touches" in e) {
-          if (e.touches.length > 1) return;
-          event = e.touches[0];
-        } else {
-          event = e;
-        }
-        memoryBuffer.copyWithin(0, size, size + size);
-        exports.drawAtPos(
-          event.x >>> SIZE_BIT_OFFSET,
-          event.y >>> SIZE_BIT_OFFSET
-        );
-      })
-    );
-  }
-);
+      memoryBuffer.copyWithin(0, size, size + size);
+      exports.drawAtPos(
+        event.x >>> SIZE_BIT_OFFSET,
+        event.y >>> SIZE_BIT_OFFSET
+      );
+    })
+  );
+});
 
 function rgb2bgr(rgb) {
   return ((rgb >>> 16) & 0xff) | (rgb & 0xff00) | ((rgb & 0xff) << 16);
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,7 @@
-export interface IExports extends WebAssembly.Exports {
+import loader from "@assemblyscript/loader";
+
+type LoaderExport = loader.ASUtil & Record<string, unknown>;
+export interface IExports extends LoaderExport {
   recalculateMemory: CallableFunction;
   fillRandomly: CallableFunction;
   drawAtPos: CallableFunction;
