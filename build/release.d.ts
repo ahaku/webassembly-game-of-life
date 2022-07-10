16aed21b6a4bbd39c212fec82858a81f07156883
ahaku <avznn@yandex.ru>
2022-07-10 15:36:33 +0300

    Updates

---
+++ build/release.d.ts
@@ -0,0 +1,16 @@
+/**
+ * src/assembly/index/init
+ * @param w `i32`
+ * @param h `i32`
+ */
+export declare function init(w: number, h: number): void;
+/**
+ * src/assembly/index/step
+ */
+export declare function step(): void;
+/**
+ * src/assembly/index/drawAtPos
+ * @param x `u32`
+ * @param y `u32`
+ */
+export declare function drawAtPos(x: number, y: number): void;
